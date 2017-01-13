4319da12bfa49807510dd5079102bb0d7a4bb86e
chandu17297 <chandu941729@gmail.com>
2017-01-13 16:09:02 -0500

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,118 @@
+
+# coding: utf-8
+
+# In[1]:
+
+#dictionary
+chandra = {'1':'shekar'}
+
+
+# In[2]:
+
+chandra
+
+
+# In[3]:
+
+#getting the value stored in particular key
+chandra['1']
+
+
+# In[7]:
+
+#dictionar can store array of values
+chan={'k1':'1' , 'k2':'2' , 'k3':'3' , 'k4':[9,2,5,3]}
+
+
+# In[9]:
+
+chan['k4']
+
+
+# In[13]:
+
+chan['k4'][2]
+
+
+# In[14]:
+
+#values stored can be changed not the key values
+chan['k4'][0]+2
+
+
+# In[20]:
+
+chan1={'k1':'a','k2':'A','k3':['D','A','W','N']}
+
+
+# In[21]:
+
+chan1['k3'][3]
+
+
+# In[22]:
+
+#change of case in dictionary
+chan1['k3'][3].lower()
+
+
+# In[26]:
+
+chan1['k3'][1]='ok'
+
+
+# In[27]:
+
+chan1['k3'][1]
+
+
+# In[29]:
+
+chan1['k1']=[1,2,3]
+
+
+# In[30]:
+
+chan1
+
+
+# In[38]:
+
+#nested dictionary
+chan2={'a1':{'b1':[8,'a',4]}}
+
+
+# In[39]:
+
+chan2['a1']['b1']
+
+
+# In[34]:
+
+chan1.keys()
+
+
+# In[36]:
+
+chan1.values()
+
+
+# In[37]:
+
+chan1.items()
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
