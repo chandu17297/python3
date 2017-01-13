f49d464d8e2c62f7c953f0f1cbd31af671ddafaa
chandu17297 <chandu941729@gmail.com>
2017-01-13 18:59:13 -0500

    Add files via upload

---
+++ range.py
@@ -0,0 +1,36 @@
+
+# coding: utf-8
+
+# In[3]:
+
+x=range(1,6)
+
+
+# In[5]:
+
+type(x)
+
+
+# In[6]:
+
+for i in range(6):
+    print(i)
+
+
+# In[7]:
+
+for i in range(2,6):
+    print(i)
+
+
+# In[8]:
+
+#start,stop, indexing:
+for i in range(1,20,2):
+    print(i)
+
+
+# In[ ]:
+
+
+
