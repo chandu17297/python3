543a8ad4aa3da8459a82d9f052477316d48e833c
chandu17297 <chandu941729@gmail.com>
2017-01-17 05:04:32 -0500

    Add files via upload
    to check whether the given string is palindrome or not

---
+++ palindrome.py
@@ -0,0 +1,8 @@
+name=input('enter the string to be checked for palinndrome')
+name=name.casefold()
+reversedname=reversed(name)
+print(reversedname)
+if(list(name)==list(reversedname)):
+    print(name, 'is a palindrome')
+else:
+    print(name, 'is not a palindrome')
