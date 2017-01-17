0358fcdd075daf1c74c47e47e16f30ba4181cdf3
chandu17297 <chandu941729@gmail.com>
2017-01-17 04:36:54 -0500

    Add files via upload

---
+++ evenodd.py
@@ -0,0 +1,7 @@
+#write a program for even and odd number
+
+for i in range(0,20):
+    if(i%2==0):
+        print(i, 'is even number')
+    else:
+        print(i, 'is odd number')
---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+#write a program for a fibonnaci series
+
+k=0
+j=1
+for i in range(0,10):
+    sum=k+j
+    print(sum)
+    k=j
+    j=sum
+
+
