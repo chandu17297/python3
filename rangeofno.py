5d0aab093cf6907a84dbba2861635ec340747ec2
chandu17297 <chandu941729@gmail.com>
2017-01-17 05:30:34 -0500

    Add files via upload

---
+++ rangeofno.py
@@ -0,0 +1,12 @@
+high=100
+low=1
+
+def number(num):
+    if(num>=low and num<=high ):
+        print('number is within the range')
+    else:
+        print('number is out of range')
+
+
+number(15)
+number(235)
